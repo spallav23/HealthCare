1f19272a69b96f8ba124449dd8d19bd3891636d4
spallav23 <145319192+spallav23@users.noreply.github.com>
2024-07-27 01:33:34 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -47,7 +47,8 @@ let a;
 const { MongoClient, ObjectId } = require("mongodb");
 const { log } = require('console')
 const { fail } = require('assert')
-const uri = "mongodb+srv://spp23102003:spp23102003@cluster0.vhincym.mongodb.net/";
+// Add your Database url
+const uri = "";
 const client = new MongoClient(uri);
 
 const database = client.db("Healthcare");
